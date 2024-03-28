217f1919f954b6b62bc25b2ae7b8264569900e30
muratcayir <mrtcyr12@hotmail.com>
2024-03-28 17:42:29 +0300

    Initial commit: Add package.json and tsconfig.json

---
+++ auth/src/index.ts
@@ -0,0 +1,12 @@
+import express from 'express'
+
+
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`App started on port ${port}`)
+})
