dd223818df88e854c567660ea7e2c6cd652ae564
muratcayir <mrtcyr12@hotmail.com>
2024-03-29 00:45:40 +0300

    feat(skaffold): Configure Skaffold for development with Docker and Kubernetes

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,6 +5,9 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+app.get("/api/users/currentuser", (req, res) => {
+    res.send("Boutique English!");
+  });
 
 const port = 3000;
 app.listen(port, () => {
